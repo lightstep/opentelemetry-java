ae3a01308582e08cec839ffc346b3a3d3808de54
Tim Nevolin <tnevolin@gmail.com>
2021-10-14 18:15:48 -0400

    Moved HTTP/JSON classes to appropriate package.

--- exporters/otlp-http-json/trace/src/test/java/io/opentelemetry/exporter/otlp/http/json/trace/OtlpHttpJsonSpanExporterTest.java
+++ exporters/otlp-http-json/trace/src/test/java/io/opentelemetry/exporter/otlp/http/json/trace/OtlpHttpJsonSpanExporterTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.exporter.otlp.http.trace;
+package io.opentelemetry.exporter.otlp.http.json.trace;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
