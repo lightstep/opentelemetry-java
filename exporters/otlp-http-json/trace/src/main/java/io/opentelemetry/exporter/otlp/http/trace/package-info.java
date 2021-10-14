441a6977c17fdf510ef56424579baa1d1f0dc836
Tim Nevolin <tnevolin@gmail.com>
2021-10-14 16:54:03 -0400

    Created new subproject for otlp-http-json #2.

--- exporters/otlp-http-json/trace/src/main/java/io/opentelemetry/exporter/otlp/http/trace/package-info.java
+++ exporters/otlp-http-json/trace/src/main/java/io/opentelemetry/exporter/otlp/http/trace/package-info.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/** OpenTelemetry exporter which sends span data to OpenTelemetry collector via OTLP HTTP. */
+/** OpenTelemetry exporter which sends span data to OpenTelemetry collector via OTLP HTTP in JSON format. */
 @ParametersAreNonnullByDefault
 package io.opentelemetry.exporter.otlp.http.trace;
 
